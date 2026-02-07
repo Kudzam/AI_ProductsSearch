d50d57348da150e5cb30e59a3cfbd4f17e722710
Kudzam <kuda.zambuko@yahoo.co.uk>
2026-02-07 22:22:55 +0000

    This is the creation for the variables inside our models class.

---
+++ Retail_AI_SearchApp/Models/RetailProductsModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.VectorData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retail_AI_SearchApp.Models
+{
+    internal class RetailProductsModel
+    {
+        [VectorStoreKey]
+        public int ID { get; set; }
+
+        [VectorStoreData]
+        public string ProductType { get; set; }
+
+        [VectorStoreData]
+        public string ProductLabel { get; set; }
+
+        [VectorStoreData]
+        public string ProductDescription { get; set; }
+
+        [VectorStoreVector(Dimensions:384, DistanceFunction = DistanceFunction.CosineSimilarity)]
+        public ReadOnlyMemory<float> Vector { get; set; }
+
+    }
+}
